46c7630ec69a7c80d039443207a7d63c30c4b4d3
huangweiqi <askviky2010@gmail.com>
2018-08-01 01:35:54 +0800

    添加counting

---
+++ data_structures/Fundamentals/counting.py
@@ -0,0 +1,22 @@
+"""
+File: counting.py
+Prints the running times for problem sizes that double,
+using a single loop.
+"""
+import time
+
+problemSize = 1000
+print("%12s%16s" % ("Problem Size", "Iterations"))
+for count in range(5):
+    number = 0
+    # The start of the algorithm
+    work = 1
+    for j in range(problemSize):
+        for x in range(problemSize):
+            number += 1
+            work += 1
+            work -= 1
+    # The end of the algorithm
+
+    print("%12d%16.3f" % (problemSize, number))
+    problemSize *= 2
